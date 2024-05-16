b09308b07593b29523c34d0802cf650cedf4b85b
Kunal Patil <kunalkpatil007@gmail.com>
2024-05-16 06:36:35 +0530

    fixed script.js

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const form = document.querySelector("form");
-// this usecase will give you empty
-// const height = parseInt(document.querySelector('#height').value)
-
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-
   const height = parseInt(document.querySelector("#height").value);
   const weight = parseInt(document.querySelector("#weight").value);
   const results = document.querySelector("#results");
@@ -15,7 +11,6 @@ form.addEventListener("submit", function (e) {
     results.innerHTML = `Please give a valid weight ${weight}`;
   } else {
     const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    //show the result
     results.innerHTML = `<span>${bmi}</span>`;
   }
 });
